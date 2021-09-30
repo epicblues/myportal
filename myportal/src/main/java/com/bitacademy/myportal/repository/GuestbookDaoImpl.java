78c8802357e06ca7ba9c44f06c6ed5301396c1f3
KMS <epicblue@hanmail.net>
2021-09-30 10:45:25 +0900

    first db connection selectAll

---
+++ myportal/src/main/java/com/bitacademy/myportal/repository/GuestbookDaoImpl.java
@@ -0,0 +1,35 @@
+package com.bitacademy.myportal.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class GuestbookDaoImpl implements GuestbookDao {
+
+	@Autowired
+	private SqlSession sqlSession; 
+	
+	@Override
+	public List<GuestbookVo> selectAll() {
+		List<GuestbookVo> list = null;
+		list = sqlSession.selectList("guestbook.selectAll");
+		return list;
+	}
+
+	@Override
+	public int insert(GuestbookVo vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(GuestbookVo vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
